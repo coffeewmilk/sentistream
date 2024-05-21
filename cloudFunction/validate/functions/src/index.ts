0d9edb640b9bc9fd9e6e2ddbc3d2008764c78c32
Kasidit Suwannagarn <coffeewmilk@Kasidits-MacBook-Pro.local>
2024-05-22 01:18:57 +0700

    reject unauthorized call

--- cloudFunction/validate/functions/src/index.ts
+++ cloudFunction/validate/functions/src/index.ts
@@ -16,14 +16,14 @@ export const validate = onCall({timeoutSeconds: 480, region: "asia-southeast1"},
   async (request) => {
     const url = request.data.url;
     const uid = request.auth?.uid;
-    let metaData: metaData
-    let batchId: string
-    
+    let metaData: metaData;
+    let batchId: string;
+
     try {
       metaData = await urlValidation(url);
     } catch (e) {
-      throw new HttpsError("invalid-argument", 
-                `The function is unable to extract video: ${e}`)
+      throw new HttpsError("invalid-argument",
+        `The function is unable to extract video: ${e}`);
     }
 
     // When called from client
@@ -31,16 +31,16 @@ export const validate = onCall({timeoutSeconds: 480, region: "asia-southeast1"},
       logger.log("Recieved call from uid:", uid);
       const user = new User(uid, db);
       await user.create();
-      
+
       // Does user exceed call quota?
       if (await user.exceedQuota()) {
-        throw new HttpsError("permission-denied", "exceed Quota")
+        throw new HttpsError("permission-denied", "exceed Quota");
       }
 
       // Is user email verified?
       if (!(await user.emailVerified())) {
         throw new HttpsError("permission-denied",
-                  "Please verifiy your email!" )
+          "Please verifiy your email!" );
       }
 
       // Check if the result already exist in database
@@ -58,21 +58,14 @@ export const validate = onCall({timeoutSeconds: 480, region: "asia-southeast1"},
       try {
         batchId = await submitTask(metaData);
       } catch (e) {
-        throw new HttpsError("internal", 
-                  `Task submittion failed: ${e}`)
-        
+        throw new HttpsError("internal",
+          `Task submittion failed: ${e}`);
       }
       user.addQuery(metaData.videoId, batchId);
       return `Started new batch: ${batchId} from user call`;
+    } else {
+      throw new HttpsError("failed-precondition", "Not authenticate");
     }
-
-    try {
-      batchId = await submitTask(metaData);
-    } catch (e) {
-      throw new HttpsError("internal", 
-                `Task submittion failed: ${e}`)
-    }
-    return `Started new batch: ${batchId} from server call`;
   }
 
 );
